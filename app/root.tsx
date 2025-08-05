6fe446bbce55862a40a767d4568e2191d23cf84f
tunkstun <johnwick4learning@gmail.com>
2025-08-05 20:04:53 +0530

    ....

--- app/root.tsx
+++ app/root.tsx
@@ -48,12 +48,16 @@ export const meta: MetaFunction = () => {
 export async function loader({ request }: LoaderFunctionArgs) {
   return json({
     theme: "dark",
-    FIREBASE_CONFIG: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      appId: process.env.FIREBASE_APP_ID,
+    ENV: {
+      FIREBASE_CONFIG: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+      },
     },
   });
 }
@@ -91,9 +95,7 @@ export default function App() {
           <ScrollRestoration />
           <script
             dangerouslySetInnerHTML={{
-              __html: `window.ENV = ${JSON.stringify({
-                FIREBASE_CONFIG: useLoaderData().FIREBASE_CONFIG,
-              })}`,
+              __html: `window.ENV = ${JSON.stringify(data.ENV)};`,
             }}
           />
 
