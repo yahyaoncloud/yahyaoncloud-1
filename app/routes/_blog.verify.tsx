1decec64077c3c3c38e6e6c556a12ff0c8487c83
tunkstun <johnwick4learning@gmail.com>
2025-08-22 10:36:33 +0530

    ...

--- app/routes/_blog.verify.tsx
+++ app/routes/_blog.verify.tsx
@@ -1,6 +1,6 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card"; //a
+import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
