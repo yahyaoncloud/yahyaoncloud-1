36bcf0ae93bbd0d56386751b3a1e7da5c15c855b
tunkstun <johnwick4learning@gmail.com>
2025-08-31 02:56:05 +0530

    ...

--- app/utils/redis.server.ts
+++ app/utils/redis.server.ts
@@ -5,8 +5,8 @@ let client: ReturnType<typeof createClient> | null = null;
 export async function getRedis() {
     if (!client) {
         client = createClient({
-            username: 'default',
-            password: 'KLukpff04tweIkEJYDGkjhn4KPlf6332',
+            username: process.env.REDIS_CRED_USER,
+            password: process.env.REDIS_CRED_PASSWORD,
             socket: {
                 host: 'redis-11024.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
                 port: 11024,
